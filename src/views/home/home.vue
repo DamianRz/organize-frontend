ec954117d4a593977c9ea7844441282873526abb
Damian <damianezetiel1@gmail.com>
2019-10-31 03:28:32 -0300

    fixes in Home

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,25 +1,60 @@
-
 <template>
-  <div>
-    <v-data-table 
-    :headers="headers" 
-    :items="events" 
-    :items-per-page="5" 
-    class="elevation-1">
-    </v-data-table>
+  <div id="home">
+    <div class="info-box">
+      <h1 class="welcome font-title">Bienvenido a Oganize!</h1>
+      <p class="welcome font-text">Crea y unete a los eventos que realizan tus amigos</p>
+      <v-btn class="button-access" dark outlined @click="dialogs.signIn = true">Acceder</v-btn>
+      <v-btn class="button-access" dark outlined @click="dialogs.signUp = true">Registrarse</v-btn>
+    </div>
+    <v-img height="100%" :aspect-ratio="16/9" :src="require('../../assets/banner.jpg')"></v-img>
+
+    <!-- register dialog -->
+    <v-dialog class="register-dialog" v-model="dialogs.signUp" width="500">
+      <v-card>
+        <v-card-title>Cree una cuenta para iniciar en Organize</v-card-title>
+        <v-card-text>
+          <div class="login-fields">
+            <v-text-field v-model="signUpData.email" class="field" label="email"></v-text-field>
+            <v-text-field v-model="signUpData.username" class="field" label="username"></v-text-field>
+            <v-text-field v-model="signUpData.password" class="field" label="password"></v-text-field>
+          </div>
+          <p class="font-error">{{ signUpErrors }}</p>
+          <div class="login-footer">
+            <v-btn @click="register()" class="button">Crear cuenta</v-btn>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
+    <!-- login dialog  -->
+    <v-dialog class="login-dialog" v-model="dialogs.signIn" width="500">
+      <v-card>
+        <v-card-title>Acceda para utilizar Organize</v-card-title>
+        <v-card-text>
+          <div class="login-fields">
+            <v-text-field v-model="signInData.email" class="field" label="email"></v-text-field>
+            <v-text-field v-model="signInData.password" class="field" label="password"></v-text-field>
+          </div>
+          <p class="font-error">{{ signInErrors }}</p>
+          <div class="login-footer">
+            <v-btn @click="login()" class="button">Acceder</v-btn>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
   </div>
 </template>
- 
 <script lang='ts'>
 import HomeCode from "./homeCode";
+import "./homeStyle.scss";
 import "../../styles/fonts.scss";
-import { Component, Vue } from "vue-property-decorator";
-// import CustomTable from "@/components/CustomTable/CustomTable.vue";
+import { Component } from "vue-property-decorator";
+import { required, minLength, between } from 'vuelidate/lib/validators';
 
 @Component({
-  components: {
-    // CustomTable
-  }
+  components: {}
 })
-export default class Home extends HomeCode {}
+export default class Home extends HomeCode {
+}
 </script>
--- src/views/home/homeCode.ts
+++ src/views/home/homeCode.ts
@@ -1,29 +1,53 @@
 import { Vue } from 'vue-property-decorator';
+import IntegrationBackend from '../../libs/IntegrationBackend';
+import ResultObject from '@/models/ResultObject';
 export default class HomeCode extends Vue {
-  private headers: any[] = [
-    {
-      text: "Dessert (100g serving)",
-      align: "left",
-      sortable: false,
-      value: "name",
-    },
-    { text: "Calories", value: "calories" },
-    { text: "Fat (g)", value: "fat" },
-    { text: "Carbs (g)", value: "carbs" },
-    { text: "Protein (g)", value: "protein" },
-    { text: "Iron (%)", value: "iron" }
-  ];
+  private backend: IntegrationBackend = new IntegrationBackend();
 
-  private event: string = "sfdsafdsfdsfds";
+  private signInData: any = {
+    email: '',
+    password: '',
+  }
+  private signInErrors = '';
 
-  private events: any[] = [
-    {
-      name: "Frozen Yogurt",
-      calories: 159,
-      fat: 6.0,
-      carbs: 24,
-      protein: 4.0,
-      iron: "1%",
+  private signUpData: any = {
+    username: '',
+    email: '',
+    password: ''
+  }
+  private signUpErrors = '';
+
+  private dialogs: any = {
+    signIn: false,
+    signUp: false
+  }
+
+  async register() {
+    // check success input data in register dialog
+    const result: any = await this.backend.send('post:signUp', { user: this.signUpData })
+    if (result.statusCode == 200) {
+      this.dialogs.signUp = false;
+      const userData = {
+        id: result.value.id,
+        token: result.value.token,
+        username: this.signUpData.username,
+      }
+      this.$store.commit('userInfo', userData)
+      this["$router"].push('/Events');
+    } else {
+      this.signUpErrors = 'No debe dejar los campos vacios'
     }
-  ];
+  }
+
+  async login() {
+    // check success input data in login dialog
+    const result: any = await this.backend.send('post:signIn', { user: this.signInData });
+    const userData = {
+      id: result.value.id,
+      token: result.value.token,
+      username: result.value.username,
+    }
+    this.$store.commit('userInfo', userData);
+    this["$router"].push('/Events');
+  }
 }
