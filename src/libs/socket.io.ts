c14ef719f8610cd333fe590dbefb5be8eeb638bb
Damian <damianezetiel1@gmail.com>
2019-10-31 03:27:05 -0300

    remove inncesary files

--- src/libs/socket.io.ts
+++
@@ -1,9 +0,0 @@
-// import io from 'socket.io-client';
-import io from 'socket.io';
-const socket = io('http://localhost:3000');
-
-socket.on('welcome', (response: any) => {
-  console.log(response)
-});
-
-export default {}
