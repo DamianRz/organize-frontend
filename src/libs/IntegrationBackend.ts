b428859f3e857f5c26ea4a1116e5c22f7933072f
Damian <damianezetiel1@gmail.com>
2019-11-12 04:44:05 -0300

    solved problem with fetch

--- src/libs/IntegrationBackend.ts
+++ src/libs/IntegrationBackend.ts
@@ -1,17 +1,14 @@
 import { Vue } from "vue-property-decorator";
 
 export default class IntegrationBackend extends Vue {
-  
+
   async send(event: string, json: any) {
-    return new Promise((resolve, reject) => {
+    // console.log(event,json)
+    return await new Promise((resolve, reject) => {
       this.$socket.client.emit(event, json);
-      this.$socket.$subscribe(event, (data: any) => {
-        if (data) {
-          return resolve(data);
-        }
+      this.$socket.client.addEventListener(event, (data: any) => {
+        return resolve(data);
       });
-    }).catch((err: any) => {
-      return err;
     });
   }
 }
