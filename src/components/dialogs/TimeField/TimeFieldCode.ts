d9885f8ef5e78bf3a570c6345846cd873f11664b
Damian <damianezetiel1@gmail.com>
2019-11-26 07:05:18 -0300

    add TimeField component

---
+++ src/components/dialogs/TimeField/TimeFieldCode.ts
@@ -0,0 +1,12 @@
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class TimeFieldCode extends Vue {
+  // date
+  menu = false;
+  date = "";
+  // time
+  time= null;
+  menu2= false;
+  modal2= false;
+}
