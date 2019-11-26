b4a4d53596ce61d91bdd6c33198ea8169fa03923
Damian <damianezetiel1@gmail.com>
2019-11-26 07:04:10 -0300

    fixes in login, main page style

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,60 +1,118 @@
-<template>
-  <div id="home">
-    <div class="info-box">
-      <h1 class="welcome font-title">Bienvenido a Oganize!</h1>
-      <p class="welcome font-text">Crea y unete a los eventos que realizan tus amigos</p>
-      <v-btn class="button-access" dark outlined @click="dialogs.signIn = true">Acceder</v-btn>
-      <v-btn class="button-access" dark outlined @click="dialogs.signUp = true">Registrarse</v-btn>
-    </div>
-    <v-img height="100%" :aspect-ratio="16/9" :src="require('../../assets/banner.jpg')"></v-img>
+<template transition="slide-x-transition">
+  <div id="homePage">
+    <v-stepper v-model="wizard" class="stepper">
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <!-- SIGN IN -->
+          <v-img
+            class="back-img"
+            height="100%"
+            :aspect-ratio="16/9"
+            :src="require('../../assets/banner.jpg')"
+          ></v-img>
+          <div class="box-access">
+            <h4 class="font-title title">Acceda para utilizar Organize</h4>
+            <v-text-field
+              v-model="newUser.email"
+              :error="v.get('newUser.email') != ''"
+              :error-messages="v.get('newUser.email')"
+              class="field"
+              label="Email"
+            ></v-text-field>
+            <v-text-field
+              v-model="newUser.password"
+              :error="v.get('newUser.password') != ''"
+              :error-messages="v.get('newUser.password')"
+              class="field"
+              type="password"
+              label="Contraseña"
+            ></v-text-field>
 
-    <!-- register dialog -->
-    <v-dialog class="register-dialog" v-model="dialogs.signUp" width="500">
-      <v-card>
-        <v-card-title>Cree una cuenta para iniciar en Organize</v-card-title>
-        <v-card-text>
-          <div class="login-fields">
-            <v-text-field v-model="signUpData.email" class="field" label="email"></v-text-field>
-            <v-text-field v-model="signUpData.username" class="field" label="username"></v-text-field>
-            <v-text-field v-model="signUpData.password" class="field" label="password"></v-text-field>
-          </div>
-          <p class="font-error">{{ signUpErrors }}</p>
-          <div class="login-footer">
-            <v-btn @click="register()" class="button">Crear cuenta</v-btn>
-          </div>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
+            <div class="footer">
+              <v-btn @click="signIn()" class="button" color="#bc7b3c">Acceder</v-btn>
+            </div>
 
-    <!-- login dialog  -->
-    <v-dialog class="login-dialog" v-model="dialogs.signIn" width="500">
-      <v-card>
-        <v-card-title>Acceda para utilizar Organize</v-card-title>
-        <v-card-text>
-          <div class="login-fields">
-            <v-text-field v-model="signInData.email" class="field" label="email"></v-text-field>
-            <v-text-field v-model="signInData.password" class="field" label="password"></v-text-field>
+            <div class="signup-button">
+              <p>No tiene cuenta? Registrese aqui</p>
+              <v-btn @click="goToStep(2)" color="#bc7b3c">
+                Registrase Aqui
+                <v-icon>people</v-icon>
+              </v-btn>
+            </div>
           </div>
-          <p class="font-error">{{ signInErrors }}</p>
-          <div class="login-footer">
-            <v-btn @click="login()" class="button">Acceder</v-btn>
-          </div>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
+        </v-stepper-content>
 
+        <v-stepper-content step="2">
+          <!-- SIGN UP -->
+          <v-img
+            class="back-img"
+            height="100%"
+            :aspect-ratio="16/9"
+            :src="require('../../assets/banner.jpg')"
+          ></v-img>
+          <div class="box-access register-box">
+            <div class="login-fields">
+              <h4 class="font-title title">Registrese Aqui</h4>
+
+              <v-text-field
+                v-model="newUser.username"
+                :error="v.get('newUser.username') != ''"
+                :error-messages="v.get('newUser.username')"
+                class="field"
+                label="Nombre de Usuario"
+              ></v-text-field>
+
+              <v-text-field
+                v-model="newUser.email"
+                :error="v.get('newUser.email') != ''"
+                :error-messages="v.get('newUser.email')"
+                class="field"
+                label="Email"
+              ></v-text-field>
+
+              <v-text-field
+                v-model="newUser.password"
+                :error="v.get('newUser.password') != ''"
+                :error-messages="v.get('newUser.password')"
+                class="field"
+                label="Contraseña"
+                type="password"
+              ></v-text-field>
+
+              <v-text-field
+                v-model="newUser.password2"
+                :error="v.get('newUser.password2') != ''"
+                :error-messages="v.get('newUser.password2')"
+                class="field"
+                label="Repita contraseña"
+                type="password"
+              ></v-text-field>
+            </div>
+            <!-- <p class="font-error font-text">{{ signUpErrors }}</p> -->
+            <div class="footer">
+              <v-btn small @click="goToStep(1)" color="#bc7b3c" class="mr-3">
+                <v-icon>arrow_back</v-icon>Volver
+              </v-btn>
+              <v-btn @click="signUp()" color="#bc7b3c" class="button">Registrarse</v-btn>
+            </div>
+          </div>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
   </div>
 </template>
-<script lang='ts'>
+
+<script lang="ts">
+//code
 import HomeCode from "./homeCode";
+//style
 import "./homeStyle.scss";
 import "../../styles/fonts.scss";
+//components
 import { Component } from "vue-property-decorator";
-import { required, minLength, between } from 'vuelidate/lib/validators';
 
 @Component({
   components: {}
 })
-export default class Home extends HomeCode {
-}
+export default class Home extends HomeCode {}
 </script>
--- src/views/home/homeCode.ts
+++ src/views/home/homeCode.ts
@@ -1,53 +1,106 @@
-import { Vue } from 'vue-property-decorator';
-import IntegrationBackend from '../../libs/IntegrationBackend';
-import ResultObject from '@/models/ResultObject';
-export default class HomeCode extends Vue {
+import vue from 'vue';
+import Validation from "../../utils/Validation";
+import IntegrationBackend from '../../utils/IntegrationBackend';
+
+// Models
+// import User from '@/models/usuario';
+
+export default class HomeCode extends vue {
+
+  // Integration Backend
   private backend: IntegrationBackend = new IntegrationBackend();
 
-  private signInData: any = {
-    email: '',
-    password: '',
-  }
-  private signInErrors = '';
+  // my litte class of validation
+  private v: Validation = new Validation();
 
-  private signUpData: any = {
+  private newUser = {
     username: '',
-    email: '',
-    password: ''
-  }
-  private signUpErrors = '';
+    password: '',
+    password2: '',
+    email: ''
+  };
 
-  private dialogs: any = {
-    signIn: false,
-    signUp: false
-  }
+  // vars used for validation into the user
+  // [ field-name, type: string, int ]
+  private signInFields: any = {
+    objectName: 'newUser',
+    fields: [
+      ['password', 'string'],
+      ['email', 'string'],
+    ]
+  };
+  private signUpFields: any = {
+    objectName: 'newUser',
+    fields: [
+      ['username', 'string'],
+      ['password', 'string'],
+      ['email', 'string'],
+    ]
+  };
 
-  async register() {
-    // check success input data in register dialog
-    const result: any = await this.backend.send('post:signUp', { user: this.signUpData })
-    if (result.statusCode == 200) {
-      this.dialogs.signUp = false;
-      const userData = {
-        id: result.value.id,
-        token: result.value.token,
-        username: this.signUpData.username,
+  // control step visible in the stepper
+  private wizard = 1;
+
+  async signUp() {
+    if (this.v.validateFields(this.newUser, [this.signUpFields])) {
+      if (this.newUser.password == this.newUser.password2) {
+        let userData = {
+          username: this.newUser.username,
+          passwd: this.newUser.password,
+        }
+        try {
+          // Integration Backend POST user send()
+          const result: any = await this.backend.send('post:signUp', { user: userData });
+          if (result.statusCode == 200) {
+            let user = {
+              id: result.value.id,
+              username: userData.username,
+            }
+            // save in the store the user data
+            this['$store'].commit('userInfo', user)
+            // goto Identification page
+            this["$router"].push('/Events');
+          } else {
+            console.error(result);
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        alert('Las contraseñas no coinciden');
       }
-      this.$store.commit('userInfo', userData)
-      this["$router"].push('/Events');
-    } else {
-      this.signUpErrors = 'No debe dejar los campos vacios'
     }
   }
 
-  async login() {
-    // check success input data in login dialog
-    const result: any = await this.backend.send('post:signIn', { user: this.signInData });
-    const userData = {
-      id: result.value.id,
-      token: result.value.token,
-      username: result.value.username,
+  async signIn() {
+    if (this.v.validateFields(this.newUser, [this.signInFields])) {
+      let userData = {
+        email: this.newUser.email,
+        password: this.newUser.password,
+      }
+      try {
+        // Integration Backend POST user send()
+        const result: any = await this.backend.send('post:signIn', { user: userData });
+        if (result.statusCode == 200) {
+          let user = {
+            id: result.value.id,
+            username: result.value.username
+          }
+          // save in the store the user data
+          this['$store'].commit('userInfo', userData)
+          // goto Identification page
+          this["$router"].push('/Events');
+        } else {
+          console.error(result);
+        }
+      } catch (error) {
+        alert('El usuario o la contraseña no son correctas');
+      }
     }
-    this.$store.commit('userInfo', userData);
-    this["$router"].push('/Events');
+  }
+
+  private goToStep(index: number) {
+    this.v.clearFails();
+    this.wizard = index;
   }
 }
