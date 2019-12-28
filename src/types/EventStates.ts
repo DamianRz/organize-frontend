3ed744fc5dc049fbbffbc836a6767a22e6bcd408
Damian <damianezetiel1@gmail.com>
2019-12-28 20:36:44 -0300

    add states const of event

---
+++ src/types/EventStates.ts
@@ -0,0 +1,4 @@
+export const STATE_ACTIVE: string = 'ACTIVE';
+export const STATE_HIDE: string = 'HIDE';
+export const STATE_FINALIZED: string = 'FINALIZE';
+export const STATE_CANCELLED: string = 'CANCELLED';
