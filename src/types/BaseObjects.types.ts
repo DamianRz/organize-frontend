37d3097f5f1f69ec189e5c0890a5035014f15510
Damian <damianezetiel1@gmail.com>
2020-01-21 01:55:51 -0300

    fix baseObject type

--- src/types/BaseObjects.types.ts
+++ src/types/BaseObjects.types.ts
@@ -4,50 +4,46 @@ import IOption from '../types/Option.type';
 import { IQuestion } from '../types/Question.type';
 import IQuestionnaire from '../types/Questionnaire.type';
 
-export default class BaseObjects {
-
-  public BASE_EVENT: IEvent = {
-    id: -1,
-    name: '',
-    location: '',
-    startDate: '',
-    startHour: '',
-    endDate: '',
-    endHour: '',
-    description: '',
-    guestsNumber: 0,
-    created: '',
-    state: true,
-  }
-
-  public BASE_USERSTORE: IUserStore = {
-    id: -1,
-    username: '',
-    token: ''
-  }
-
-  public BASE_OPTION: IOption = {
-    id: -1,
-    idUser: -1,
-    idQuestion: -1,
-    name: '',
-    cost: 0.0
-  }
-
-  public BASE_QUESTION: IQuestion = {
-    id: -1,
-    idType: -1,
-    name: '',
-    category: '',
-    options: []
-  }
-
-  public BASE_QUESTIONNAIRE: IQuestionnaire = {
-    id: -1,
-    idUser: -1,
-    name: '',
-    category: '',
-    questions: []
-  }
+export const BASE_EVENT = {
+  id: -1,
+  name: '',
+  location: '',
+  startDate: '',
+  startHour: '',
+  endDate: '',
+  endHour: '',
+  description: '',
+  guestsNumber: 0,
+  created: '',
+  state: true,
 }
 
+export const BASE_USERSTORE = {
+  id: -1,
+  username: '',
+  token: ''
+}
+
+export const BASE_OPTION = {
+  id: -1,
+  idUser: -1,
+  idQuestion: -1,
+  name: '',
+  cost: 0.0
+}
+
+export const BASE_QUESTION = {
+  id: -1,
+  idType: -1,
+  name: '',
+  category: '',
+  options: []
+}
+
+export const BASE_QUESTIONNAIRE = {
+  id: -1,
+  idUser: -1,
+  name: '',
+  category: '',
+  questions: []
+}
