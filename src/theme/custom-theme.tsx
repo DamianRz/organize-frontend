24c6bbf76d78bd16af2769e33edcf04f5ebafefb
damian <damianezetiel1@gmail.com>
2020-10-27 18:04:43 -0300

    add theme

---
+++ src/theme/custom-theme.tsx
@@ -0,0 +1,21 @@
+export const customTheme = {
+    text: {
+        fontFamily: ['Open Sans', 'sans-serif'],
+        fontSize: {
+            bigTitle: 30,
+            title: 25,
+            text: 16,
+        },
+        color: {
+            dark: '#bebebe',
+            light: '#303030'
+        }
+    },
+    pallette: {
+        light: '#d8d7d7',
+        dark: '#2c2c2cb6',
+        primary: '#a5a262',
+        secondary: '#c3bf6f',
+    },
+    borderRadius: 4
+}
